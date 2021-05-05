f108f90a4d86c7983f16cf7f94e5c87a8be3e9d6
Baskoekkoek1 <baskoekkoek@gmail.com>
2021-05-05 19:16:27 +0200

    add group column

--- migrations/20210504141025-create-match.js
+++ migrations/20210504141025-create-match.js
@@ -1,36 +1,36 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('matches', {
+    await queryInterface.createTable("matches", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       homeTeam: {
-        type: Sequelize.JSON
+        type: Sequelize.JSON,
       },
       awayTeam: {
-        type: Sequelize.JSON
+        type: Sequelize.JSON,
       },
       homeScore: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       awayScore: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('matches');
-  }
+    await queryInterface.dropTable("matches");
+  },
 };
